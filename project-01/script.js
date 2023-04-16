9f77cc7672d21398052c8781f1167dfc8c5cd10f
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-04-16 13:22:07 +0200

    Delete project-01 directory

--- project-01/script.js
+++
@@ -1,6 +0,0 @@
-function flipCoin() {
-  const result = Math.floor(Math.random() * 2);
-
-  const coin = document.getElementById("coin");
-  coin.className = result === 0 ? "coin eagle" : "coin tail";
-}
