7935eb92a29ea58d253bf01bf76bd0c1d6215092
Vin-Dave <vin.dave.9702@gmail.com>
2024-02-11 21:51:02 +0100

    code added

--- new-page/src/App.jsx
+++ new-page/src/App.jsx
@@ -1,8 +1,12 @@
+import EmptyProject from "./components/EmptyProject/EmptyProject";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <EmptyProject />
+    </main>
   );
 }
 
