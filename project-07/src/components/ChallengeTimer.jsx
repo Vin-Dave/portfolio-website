9b0e9dc5659bdb2ee588cf36e07dcf68ce8b91de
Vin-Dave <vin.dave.9702@gmail.com>
2024-02-23 06:06:57 +0100

    code added

--- project-07/src/components/ChallengeTimer.jsx
+++ project-07/src/components/ChallengeTimer.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { faL } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState } from "react";
 
 export default function ChallengeTimer({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
+  const time = useRef();
+  function handleStartAction() {
+    time.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStopAction() {
+    clearInterval(time.current);
+    setTimerStarted(false);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -12,7 +25,7 @@ export default function ChallengeTimer({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={() => {}}>
+        <button onClick={timerStarted ? handleStopAction : handleStartAction}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
