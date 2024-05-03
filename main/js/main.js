cd68f513bf63c58fa0dbbcdb5b21bf7cd8ca33bc
Vin-Dave <vin.dave.9702@gmail.com>
2024-05-03 12:50:42 +0200

    code added

--- main/js/main.js
+++ main/js/main.js
@@ -8,10 +8,48 @@ const toggleNav = () => {
 mobileNav.addEventListener("click", () => toggleNav());
 
 window.addEventListener("scroll", function () {
-  var nav = document.querySelector(".nav");
+  let nav = document.querySelector(".nav");
   if (window.scrollY > 100) {
     nav.classList.add("transparent");
   } else {
     nav.classList.remove("transparent");
   }
+});
+
+$(document).ready(function () {
+  $("#carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  // Obsługa przycisków nawigacyjnych
+  $(".carousel-control.prev").click(function () {
+    $("#carousel").slick("slickPrev");
+  });
+
+  $(".carousel-control.next").click(function () {
+    $("#carousel").slick("slickNext");
+  });
 });
