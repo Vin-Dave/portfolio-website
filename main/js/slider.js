bf3fa882d94181eec7fc0e633309507ae01edb7c
Vin-Dave <vin.dave.9702@gmail.com>
2024-05-16 21:51:50 +0200

    code added

---
+++ main/js/slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("#carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".carousel-control.prev").click(function () {
+    $("#carousel").slick("slickPrev");
+  });
+
+  $(".carousel-control.next").click(function () {
+    $("#carousel").slick("slickNext");
+  });
+});
