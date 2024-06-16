6d2297540be0e75fb8606e7a74de96a77d17edbf
Vin-Dave <vin.dave.9702@gmail.com>
2024-06-16 18:59:43 +0200

    code added animation

--- main/js/main.js
+++ main/js/main.js
@@ -26,3 +26,28 @@ document
 document.querySelector(".close-button").addEventListener("click", function () {
   document.getElementById("contactPopup").style.display = "none";
 });
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".animated-section");
+  var position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.style.opacity = "1";
+    element.style.transform = "translateY(0)";
+  } else {
+    element.style.opacity = "0";
+    element.style.transform = "translateY(20%)";
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".animated-section-left");
+  var position = element.getBoundingClientRect();
+
+  if (position.left < window.innerWidth && position.right > 0) {
+    element.style.opacity = "1";
+    element.style.transform = "translateX(0)";
+  } else {
+    element.style.opacity = "0";
+    element.style.transform = "translateX(-100%)";
+  }
+});
