bba55d29aed0805e5270f8b136490aec98810ad3
Vin-Dave <vin.dave.9702@gmail.com>
2024-07-14 19:34:38 +0200

    fix label

--- main/js/slider.js
+++ main/js/slider.js
@@ -33,4 +33,15 @@ $(document).ready(function () {
   $(".carousel-control.next").click(function () {
     $("#carousel").slick("slickNext");
   });
+  $("#carousel2").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true, // włącz autoplay
+    autoplaySpeed: 2000, // interwał autoplay
+    swipe: true, // włącz przesuwanie gestami
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1 } },
+    ],
+  });
 });
