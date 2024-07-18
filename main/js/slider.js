584b3d03554c8c7a429d60ffd509660c3a254eb9
Vin-Dave <vin.dave.9702@gmail.com>
2024-07-18 16:17:56 +0200

    fix label

--- main/js/slider.js
+++ main/js/slider.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true, // włącz autoplay
-    autoplaySpeed: 2000, // interwał autoplay
+    autoplaySpeed: 4000, // interwał autoplay
     swipe: true, // włącz przesuwanie gestami
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 2 } },
